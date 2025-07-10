269ca45a3d1aeb326811ad890155ce3c17246115
debanjan <you@example.com>
2025-07-10 23:11:16 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import Blog_details from "./pages/Blog_details"
-import BlogAll from "./pages/BlogAll"
-import Book from "./pages/Book"
-import Home from "./pages/Home"
-import ImageGallery from "./pages/ImageGallery"
+import Blog_details from "./pages/Blog_details";
+import BlogAll from "./pages/BlogAll";
+import Book from "./pages/Book";
+import ContactUs from "./pages/ContactUs";
+import Home from "./pages/Home";
+import ImageGallery from "./pages/ImageGallery";
+import VideoGallery from "./pages/VideoGallery";
 
-
-const App=()=> {
- 
-
+const App = () => {
   return (
     <>
-     {/* <Home/> */}
-     {/* <Blog_details/> */}
-     {/* <Book/> */}
-     {/* <BlogAll/> */}
-     <ImageGallery/>
+      {/* <Home/> */}
+      {/* <Blog_details/> */}
+      {/* <Book/> */}
+      {/* <BlogAll/> */}
+      {/* <ImageGallery/> */}
+      <VideoGallery />
+      <ContactUs/>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/component/Videogallery/Videogallery.jsx
@@ -0,0 +1,9 @@
+
+import "./Videogallery.css"
+const Videogallery=()=>{
+    return(
+        <>
+        </>
+    )
+}
+export default Videogallery
